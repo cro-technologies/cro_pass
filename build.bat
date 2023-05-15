91293c5f3318dbea6fb0e286797f2aaca598f7ba
Lonecoder.cro <25649334+lonecoder-cro@users.noreply.github.com>
2023-05-15 14:09:40 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 cls
-rem mode con cols=60 lines=10
-title "building CroKeyGen"
+title "Building CroKeyGen"
 set name="crokeygen.spec"
 pyinstaller  --noconfirm --clean %name%
 rmdir /s /q build
